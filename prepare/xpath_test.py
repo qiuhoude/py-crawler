e16a2ab3375bd067202881b239f2136733809925
qiuhoude <3617150@qq.com>
2019-11-29 20:45:57 +0800

    boss直聘 列表例子

---
+++ prepare/xpath_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from lxml import etree
+
+
+def TestBossJob():
+    # etree.HTML() # 解析字符串使用
+    html_parser = etree.HTMLParser(encoding='utf-8')
+    html = etree.parse('boss_golang.html', parser=html_parser)
+    jobDivs = html.xpath('//div[@class="job-primary"]')
+    for index, jobDiv in enumerate(jobDivs):
+        infoP = jobDiv.xpath('./div[1]')[0]
+        jobTitle = infoP.xpath('.//div[@class="job-title"]/text()')[0]
+        salary = infoP.xpath('.//span[@class="red"]/text()')[0]
+        link = infoP.xpath('./h3/a/@href')[0]
+        company = jobDiv.xpath('./div[2]//a/text()')[0]
+        print(index, jobTitle, salary, company, "https://www.zhipin.com" + link)
+
+
+if __name__ == '__main__':
+    TestBossJob()
